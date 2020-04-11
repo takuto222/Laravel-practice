fad94e68e88f42fc543d7a2865169aad814d057f
takahashi-takuto <takuto.ft222@gmail.com>
2020-04-11 14:47:59 +0000

    PHP/Laravel13の内容＋課題を追加

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,20 +1,49 @@
-<!DOCTYPE html>
-
 @extends('layouts.profile')
-
 @section('title', 'プロフィール')
 
 @section('content')
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile_create画面</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
 
-        <title>Profile</title>
-    </head>
-    <body>
-        <h1>profile_create画面</h1>
-    </body>
-</html>
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio" name="性別" value="男" /> 男　
+                            <input type="radio" name="性別" value="女" /> 女
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,19 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
+    // Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    
     // Laravel09の課題3
     // Route::get('profile/update', 'Admin\ProfileController@update');
     // Laravel09の課題4
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+    // Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    // Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 Auth::routes();
 
