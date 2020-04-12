4f2398b148d06bee12e414670eb14a18b3502245
takahashi-takuto <takuto.ft222@gmail.com>
2020-04-12 04:33:38 +0000

    Laravel/PHP14の内容を追加

--- config/database.php
+++ config/database.php
@@ -44,9 +44,10 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'mynews'),
-            'username' => env('DB_USERNAME', 'techboost'),
-            'password' => env('DB_PASSWORD', 'takuto22'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            //'unix_socket' => '/tmp/mysql.sock', // 新しく追加
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
